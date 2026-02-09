2932a0fb7f6e736cb330e3f4616f4378f1f77398
Binayak Dahit <binayakdahit28@gmail.com>
2026-02-09 23:25:40 +0545

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav className="bg-white ">
+            SanoBazardkcnk
+        </nav> 
+
+    )
+}
+
+export default Navbar
